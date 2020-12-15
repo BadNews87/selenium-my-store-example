6e62f3ffa95a977990d735d0bd265373ffad5dc8
BadNews87 <krisu876@wp.pl>
2020-12-15 18:10:20 +0100

    Configuration support added

---
+++ src/main/java/config/Configuration.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({"system:properties",
+        "classpath:general.properties"})
+public interface Configuration extends Config {
+
+    @Key("base.url")
+    String baseUrl();
+}
